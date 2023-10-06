d2c756b7b00af1491ff6f4c76af127b6d3909054
kanagusam@5 <kanagarajwhb@gmail.com>
2023-10-06 19:46:07 +0530

    header option icon added

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,18 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { SimpleLineIcons } from "@expo/vector-icons";
 
 const Header = () => {
   return (
     <View style={styles.headerContainer}>
       <Text style={styles.titleText}>WhatsDirect</Text>
+      <TouchableOpacity>
+        <SimpleLineIcons
+          name="options-vertical"
+          size={25}
+          color="black"
+          style={styles.optionIcon}
+        />
+      </TouchableOpacity>
     </View>
   );
 };
@@ -14,11 +23,16 @@ const styles = StyleSheet.create({
   headerContainer: {
     backgroundColor: "#128c7e",
     height: "12%",
-    justifyContent: "center",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
   },
   titleText: {
     fontSize: 35,
     color: "#fff",
     paddingLeft: 12,
+  },
+  optionIcon: {
+    marginRight: 15,
   },
 });
