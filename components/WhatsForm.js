d22c497d001548e3774b73042375c0c16727d529
kanagusam@5 <kanagarajwhb@gmail.com>
2023-10-17 23:09:06 +0530

    button element added

--- components/WhatsForm.js
+++ components/WhatsForm.js
@@ -1,5 +1,6 @@
-import { StyleSheet, View, Text, TextInput } from "react-native";
+import { StyleSheet, View, Text, TextInput, Button } from "react-native";
 import React from "react";
+import { SelectList } from "react-native-dropdown-select-list";
 
 const WhatsForm = () => {
   return (
@@ -9,16 +10,33 @@ const WhatsForm = () => {
         style={styles.textInput}
         placeholder="Enter Phone Number"
         keyboardType="numeric"
+        secureTextEntry={false}
       />
+      <View style={styles.buttonStyle}>
+        <Button color={"#128c7e"} title="Start chat" />
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  formContainer: {
+    flexDirection: "row",
+    marginTop: 10,
+    justifyContent: "space-between",
+    marginLeft: -8,
+  },
+  boxStyles: {
+    borderWidth: 0,
+  },
   textInput: {
     borderBottomWidth: 2,
     height: 40,
     fontSize: 20,
+  },
+  buttonStyle: {
+    marginTop: 15,
+    alignSelf: "flex-end",
   },
 });
 
