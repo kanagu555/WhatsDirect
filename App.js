72dce57f6b8ead08e30c6db7a7239f73acb8c503
kanagusam@5 <kanagarajwhb@gmail.com>
2023-10-19 22:53:23 +0530

    TouchableWithoutFeedback added for keyboard dismiss

--- App.js
+++ App.js
@@ -1,15 +1,28 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 
 export default function App() {
+  const handleKeyboardDismiss = () => {
+    Keyboard.dismiss();
+  };
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <Header />
-      <MainContent />
-    </View>
+    <TouchableWithoutFeedback onPress={handleKeyboardDismiss}>
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <Header />
+        <MainContent />
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
